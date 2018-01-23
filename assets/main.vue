219f942acc0da01abfcb1a1cdbd3e1e16f339c98
Northerchild <osarus-13@hotmail.com>
2018-01-22 23:03:04 -0500

    Se agrega la funcionalidad de borrar tareas

--- assets/main.vue
+++ assets/main.vue
@@ -1,6 +1,24 @@
 new Vue({
 	el:'#taskApp',
 	data:{
-	 nameApp: 'VueTask',
+	 nameApp: 'GrabiTask',
+	 tareas:[
+	{
+	 	titulo: 'tareas',
+	 	hecho: true
+	},
+	{
+	 	titulo: 'tareas 2',
+	 	hecho: false
+	},
+	{
+	 	titulo: 'tareas 3',
+	 	hecho: false
+	},
+	]
+},methods:{
+	eliminarTarea:function(tarea){
+	this.tareas.splice(this.tareas.indexOf(tarea),1)
+}
 }
 })
